b74782378ba3c02d0b18ec0948093297cdd628d5
0xkerem <54220763+0xkerem@users.noreply.github.com>
2023-04-28 22:06:34 +0300

    Delete price_tracker directory

--- price_tracker/src/main/java/com/price_tracker/server/repository/AlarmRepo.java
+++
@@ -1,8 +0,0 @@
-package com.price_tracker.server.repository;
-
-import com.price_tracker.server.entity.Alarm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlarmRepo extends JpaRepository<Alarm, Integer> {
-  Alarm findById(int id);
-}
