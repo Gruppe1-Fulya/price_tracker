41d23bd84999a6e589cf3fd5c56520508e62ae6a
0xkerem <keremozrtk@gmail.com>
2023-04-28 22:04:53 +0300

    missing files fixed

---
+++ price_tracker/src/main/java/com/price_tracker/server/repository/AlarmRepo.java
@@ -0,0 +1,8 @@
+package com.price_tracker.server.repository;
+
+import com.price_tracker.server.entity.Alarm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlarmRepo extends JpaRepository<Alarm, Integer> {
+  Alarm findById(int id);
+}
---
+++ price_tracker/src/main/java/com/price_tracker/server/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.price_tracker.server.service;
+
+import com.price_tracker.server.entity.Product;
+import com.price_tracker.server.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductService {
+  private final ProductRepo productRepo;
+
+  @Autowired
+  public ProductService(ProductRepo productRepo) {
+    this.productRepo = productRepo;
+  }
+
+  public Product findByURL(String URL) {
+    return productRepo.findByUrl(URL);
+  }
+
+  public Product saveProduct(Product product) {
+    return productRepo.save(product);
+  }
+
+  public void deleteProductById(int productId) {
+    productRepo.deleteById(productId);
+  }
+}
